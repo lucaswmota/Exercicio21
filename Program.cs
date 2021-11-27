6a017ca7bf8b99926982f26b69cf00f0fbffb56d
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 13:11:40 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o valor da taxa de esgoto: ");
+            double taxa = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o total da conta: ");
+            double valorConta = double.Parse(Console.ReadLine());
+
+            double percentualTaxa = (taxa / valorConta) * 100;
+
+            Console.Write("O percentual da taxa é: " + percentualTaxa + " %");
+
+            Console.ReadKey();
+
+        }
+    }
+}
